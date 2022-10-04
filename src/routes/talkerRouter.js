682659649733b42e45c4e90d1e85b40e668d9bf9
BrenoOPrado <breno.o.prado@hotmail.com>
2022-10-04 15:20:23 -0300

    Requisito 2 completo :turtle:

--- src/routes/talkerRouter.js
+++ src/routes/talkerRouter.js
@@ -5,12 +5,23 @@ const path = require('path');
 const router = express.Router();
 const talkerPath = path.resolve(__dirname,'..', 'talker.json');
 
-router.get('/', async (req, res) => {
+router.get('/', async function(_req, res) {
     const talkers = JSON.parse(await fs.readFile(talkerPath, 'utf8'));
     if(talkers.length > 0) {
         res.status(200).json(talkers);
     } else {
         res.status(200).json([]);
+    }
+});
+
+router.get('/:id', async function(req, res) {
+    const talkers = JSON.parse(await fs.readFile(talkerPath, 'utf8'));
+    const id = req.params.id;
+    const index = talkers.findIndex((item) => item.id === parseInt(id));
+    if(index < 0) {
+        res.status(404).json({ message: "Pessoa palestrante não encontrada" });
+    } else {
+        res.status(200).json(talkers[index]);
     }
 });
 
