5c2d5ddec954d43d5154768b1159d66ffe612cf3
BrenoOPrado <breno.o.prado@hotmail.com>
2022-10-04 18:39:20 -0300

    Requisito 4 completo :turtle:

---
+++ src/middleware/emailValidation.js
@@ -0,0 +1,13 @@
+module.exports = function emailValidation(req, res, next) {
+   const { email } = req.body;
+    
+    if (email === undefined || email.length <= 0) {
+      return res.status(400).json({ message: 'O campo \"email\" é obrigatório' });
+    }
+    if (!email.includes('@') || !email.endsWith('.com')) {
+      return res.status(400).json({
+         message: 'O \"email\" deve ter o formato \"email@email.com\"',
+      });
+    }
+    next();
+ };
--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const emailValidation = require('../middleware/emailValidation');
+const passwordValidation = require('../middleware/passwordValidation');
 
 const router = express.Router();
 
-router.post('/', async (_req, res) => {
+router.post('/', emailValidation, passwordValidation, async (_req, res) => {
     let token = '';
     const caracteres = 'abcdefghijklmnopqrstuvwxyz0123456789';
     for (let i = 0; i < 16; i += 1) {
